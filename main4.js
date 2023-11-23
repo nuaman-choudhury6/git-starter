465a1bed007be2ccd0240033df154d6856ff7e4c
nuaman-choudhury6 <nuamanchy66@gmail.com>
2023-11-23 15:17:46 +0530

    kkjhhg

--- main4.js
+++ main4.js
@@ -1,30 +1,40 @@
-function onsignup(event) {
-    event.preventDefault(); // Prevent the form from submitting the traditional way
-
-    // Get the form values
-    var username = document.getElementById('username').value;
-    var email = document.getElementById('email').value;
-    var password = document.getElementById('password').value;
-
-    // Create a user object
-    var user = {
-        username: username,
-        email: email,
-        password: password
-    };
-
-    // Retrieve existing users from local storage or initialize an empty array
-    var existingUsersJSON = localStorage.getItem('users');
-    var existingUsers = existingUsersJSON ? JSON.parse(existingUsersJSON) : [];
-
-    // Add the new user to the array
-    existingUsers.push(user);
-
-    // Convert the array of users to a JSON string
-    var usersJSON = JSON.stringify(existingUsers);
-
-    // Save the updated array of users to local storage
-    localStorage.setItem('users', usersJSON);
-}
-
-
+document.addEventListener('DOMContentLoaded', function () {
+    const userList = document.getElementById('userList');
+  
+    // Function to handle form submission (signup)
+    function onsignup(event) {
+      event.preventDefault(); // Prevent the default form submission
+  
+      // Get user input values
+      const username = document.getElementById('username').value;
+      const email = document.getElementById('email').value;
+      const password = document.getElementById('password').value;
+  
+      // Create a new user object
+      const user = { username, email, password };
+  
+      // Add the user to the list and update UI
+      addUserToList(user);
+    }
+  
+    // Function to add a user to the list and update UI
+    function addUserToList(user) {
+      const listItem = document.createElement('li');
+      listItem.innerHTML = `${user.username} - ${user.email} 
+        <button onclick="deleteUser(this)">Delete</button>`;
+  
+      userList.appendChild(listItem);
+    }
+  
+    // Function to delete a user
+    function deleteUser(button) {
+      const listItem = button.parentNode;
+      userList.removeChild(listItem);
+      // Additional logic can be added here, such as updating data on the server or local storage
+    }
+  
+    // Assign functions to global scope for inline event handlers
+    window.onsignup = onsignup;
+    window.deleteUser = deleteUser;
+  });
+  
